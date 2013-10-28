8fdc94633150605babf897bd6f8e549aebf4a663
Jordan Killpack <jordan@bignerdranch.com>
2013-10-28 14:25:56 -0400

    Pass those specs

--- lib/lita/handlers/down_for_everyone.rb
+++ lib/lita/handlers/down_for_everyone.rb
@@ -3,6 +3,25 @@ require "lita"
 module Lita
   module Handlers
     class DownForEveryone < Handler
+      route(/\bis (.+) down\b/i, :is_site_down, command: true, help: { "is SOME_URL down?" => "Tells you whether SOME_URL is working or not." })
+
+      def is_site_down(response)
+        site_url = response.matches[0][0]
+        api_url = api_url(response.matches[0][0]) 
+        response_html = http.get(api_url).body
+
+        if response_html.include? "looks down from here."
+          response.reply "Looks like #{site_url} is down. #{api_url}"
+        elsif response_html.include? "It's just you."
+          response.reply "It's just you: looks like #{site_url} is up. #{api_url}"
+        end
+      end
+
+      private
+
+      def api_url(site_url)
+        "http://isup.me/#{site_url}"
+      end
     end
 
     Lita.register_handler(DownForEveryone)
